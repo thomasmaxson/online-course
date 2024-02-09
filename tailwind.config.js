02e04f7e75b785caac51fd587165af7572721e02
Thomas Maxson <thomas@thomasmaxson.com>
2024-02-09 09:03:46 -0500

    Added Button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: '#7d41e1',// "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
