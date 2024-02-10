31ef93cbb815470634b76ecf7187a329fadeaf48
Thomas Maxson <thomas@thomasmaxson.com>
2024-02-10 10:23:59 -0500

    Updated side nav links

--- app/(router)/_components/SideNav.jsx
+++ app/(router)/_components/SideNav.jsx
@@ -1,23 +1,37 @@
+"use client"
+
 import { BadgeIcon, BookOpen, GraduationCap } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 function SideNav() {
 	const menu = [ 
 		{ 
 			id: 1, 
-			name: "All Courses", 
-			icon: BookOpen
+			name: 'All Courses', 
+			icon: BookOpen,
+			path: '/courses',
+			target: '_self'
 		}, { 
 			id: 2, 
-			name: "Memberships", 
-			icon: BadgeIcon
+			name: 'Memberships', 
+			icon: BadgeIcon,
+			path: '/memberships',
+			target: '_self'
 		}, { 
 			id: 3, 
-			name: "Instructors", 
-			icon: GraduationCap
+			name: 'Instructors', 
+			icon: GraduationCap,
+			path: '/instructors',
+			target: '_self'
 		}
 	];
+
+	const currentPath = usePathname();
+
+
 
 	return ( 
 		<div className='p-5 h-screen bg-white shadow-sm border-r'>
@@ -25,10 +39,10 @@ function SideNav() {
 			<hr className='mt-7' />
 			<div className='mt-8'>
 				{ menu.map( ( item, index ) => ( 
-					<div className="group flex gap-3 mt-2 p-3 text-[18px] text-gray-500 items-center cursor-pointer hover:bg-primary hover:text-white rounded-md transition-all ease-in-out duration-200">
-						<item.icon className='group-hover:animate-bounce' />
+					<Link href={ item.path } target={ item.target } className={`group flex gap-3 mt-2 p-3 text-[18px] items-center rounded-md transition-all ease-in-out duration-200 ${ currentPath.includes( item.path ) ? 'text-white bg-primary cursor-default' : 'text-gray-500 hover:text-primary hover:bg-gray-200' }`}>
+						<item.icon className={ currentPath.includes( item.path ) ? '' : 'group-hover:animate-bounce'} />
 						<h2>{ item.name }</h2>
-					</div>
+					</Link>
 				) ) }
 			</div>
 		</div>
